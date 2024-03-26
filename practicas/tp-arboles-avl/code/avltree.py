58a4be5461b03190b948218cfeb60e307f234d8e
Gonzalo Padilla <107971962+gonza2323@users.noreply.github.com>
2024-03-25 23:54:29 -0300

    Add comments to code

--- practicas/tp-arboles-avl/code/avltree.py
+++ practicas/tp-arboles-avl/code/avltree.py
@@ -11,43 +11,46 @@ class AVLNode:
     value = None
     bf = None
 
+
 # --- Ejercicio 4
 def _insertNode(currentNode, newNode):    
-    if newNode.key < currentNode.key:
-        if not currentNode.leftnode:
+    if newNode.key < currentNode.key: # Insertamos a la izquierda
+        if not currentNode.leftnode: # Si no hay hijo, pasa a ser el nuevo hijo
             currentNode.leftnode = newNode
             newNode.parent = currentNode
             return newNode.key
-        else:
+        else: # Si lo hay, llamamos a la recursión
             return _insertNode(currentNode.leftnode, newNode)
-    elif newNode.key > currentNode.key:
-        if not currentNode.rightnode:
+    elif newNode.key > currentNode.key: # Insertamos a la derecha
+        if not currentNode.rightnode: # Si no hay hijo, pasa a ser el nuevo hijo
             currentNode.rightnode = newNode
             newNode.parent = currentNode
             return newNode.key
-        else:
+        else: # Si lo hay, llamamos a la recursión
             return _insertNode(currentNode.rightnode, newNode)
     else:
         print("Error! Ya existe un elemento para la key indicada!")
-        return None
+        return None # Si ya existe la key en el árbol, devolvemos None
 
 
 def insert(t, element, key):
     if not t:
         return None
     
+    # Creamos el nuevo nodo
     newNode = AVLNode()
     newNode.key = key
     newNode.value = element
     newNode.bf = 0
 
+    # Si no hay raíz, pasa a ser la raíz
     if not t.root:
         t.root = newNode
         return key
     
-    result = _insertNode(t.root, newNode)
-    reBalance(t)
-    return result
+    key = _insertNode(t.root, newNode) # Si no, insertamos
+    reBalance(t) # Rebalanceamos
+    return key # Retornamos la key si fue exitoso (puede ser None)
 # --- end
 
 
@@ -103,8 +106,12 @@ def _deleteNode(B, node):
     if not node:
         return None
     
+    # El nodo a eliminar es reemplazado por el más pequeño de su subárbol derecho
+    # o el más grande de su subárbol izquierdo para mantener el orden
+    # _delteNode lo elimina de su posición y nos lo devuelve
     newNode = _deleteNode(B, _findSmallest(node.rightnode) or _findLargest(node.leftnode))
 
+    # Si existe un reemplazo, reemplazamos
     if newNode:
         newNode.leftnode = node.leftnode
         newNode.rightnode = node.rightnode
@@ -114,6 +121,7 @@ def _deleteNode(B, node):
             newNode.rightnode.parent = newNode
         newNode.parent = node.parent
     
+    # Apuntamos el padre al nuevo nodo
     if not node.parent:
         B.root = newNode
     elif node.parent.leftnode == node:
@@ -121,14 +129,14 @@ def _deleteNode(B, node):
     else:
         node.parent.rightnode = newNode
 
-    return node
-# --- end
+    return node # Retornamos el nodo eliminado
 
 
 def delete(B, element):
-    node = _deleteNode(B, _findNodeByValue(B.root, element))
-    reBalance(B)
-    return node.key if node else None
+    node = _deleteNode(B, _findNodeByValue(B.root, element)) # Buscamos el nodo y lo eliminamos
+    reBalance(B) # Rebalanceamos el árbol
+    return node.key if node else None # Retornamos la key
+# --- end
 
 
 def deleteKey(B, key):
@@ -153,16 +161,14 @@ def update(B, element, key):
 
 # --- Ejercicio 1
 def rotateLeft(t, node):
-    # (raíz) A -> B -> C
-
-    # Caso especial:
+    # Caso especial donde debe realizarse una rotación previa
     if node.rightnode.bf > 0:
         rotateRight(t, node.rightnode)
 
-    a = node
-    b = node.rightnode
+    a = node # raíz actual
+    b = node.rightnode # nueva raíz
 
-    # Nodo B es la nueva raíz
+    # Convertir a 'b' en la nueva raíz
     a.rightnode = None
     b.parent = a.parent
     if b.parent:
@@ -173,30 +179,30 @@ def rotateLeft(t, node):
     else:
         t.root = b
     
-    # Si B tenía hijo izquierdo, pasa a ser el
-    # hijo derecho de A
+    # Si 'b' tenía hijo izquierdo, pasa a ser el
+    # hijo derecho de 'a'
     if b.leftnode:
         a.rightnode = b.leftnode
         b.leftnode.parent = a
         b.leftnode = None
 
-    # Nodo A es el hijo izquierdo de B
+    # Nodo 'a' para a ser hijo izquierdo de 'b'
     b.leftnode = a
     a.parent = b
 
-    return b
+    return b # Retornamos la nueva raíz
 # --- end
 
 
 def rotateRight(t, node):
-    # Caso especial:
+    # Caso especial donde debe realizarse una rotación previa
     if node.leftnode.bf < 0:
         rotateLeft(t, node.leftnode)
 
-    a = node
-    b = node.leftnode
+    a = node # raíz actual
+    b = node.leftnode # nueva raíz
 
-    # Nodo B es la nueva raíz
+    # Nodo 'b' es la nueva raíz
     a.leftnode = None
     b.parent = a.parent
     if b.parent:
@@ -207,18 +213,18 @@ def rotateRight(t, node):
     else:
         t.root = b
     
-    # Si B tenía hijo derecho, pasa a ser el
-    # hijo izquierdo de A
+    # Si 'b' tenía hijo derecho, pasa a ser el
+    # hijo izquierdo de 'a'
     if b.rightnode:
         a.leftnode = b.rightnode
         b.rightnode.parent = a
         b.rightnode = None
     
-    # Nodo A es el hijo derecho de B
+    # Nodo 'a' para a ser hijo derecho de 'b'
     b.rightnode = a
     a.parent = b
 
-    return b
+    return b # Retornamos la nueva raíz
     
 
 # --- Ejercicio 2
@@ -226,6 +232,8 @@ def calculateBalance(AVLTree):
     if not AVLTree:
         return None
     
+    # Calcula el balance factor de cada nodo, calculando
+    # previamente la altura de cada sub árbol. Orden O(n)
     def calculateNodeBalance(node):
         if not node:
             return 0
@@ -236,40 +244,45 @@ def calculateBalance(AVLTree):
         return max(leftHeight, rightHeight) + 1
     
     calculateNodeBalance(AVLTree.root)
-    return AVLTree
+    return AVLTree # Retornamos el árbol
 # --- end
 
 
 # --- Ejercicio 3
+# Balancea un nodo y sus hijos
+# Retorna True si ocurrió un balanceo de forma exitosa
+# Si no ocurrieron, o fallaron, retorna False
 def reBalanceNode(t, node):
     if not node:
         return False
     
+    # Balanceamos los subárboles del nodo
+    # Si un nodo inferior fue balanceado, por propiedades
+    # de los AVL ya no es necesario balancear este nodo
     wasReBalanced = reBalanceNode(t, node.leftnode) or reBalanceNode(t, node.rightnode)
 
     if wasReBalanced:
         return True
     
-    if abs(node.bf) < 2:
+    if abs(node.bf) < 2: # Nodo balanceado
         return False
 
     if node.bf == 2:
-        rotateRight(t, node)
+        return rotateRight(t, node) # Balanceamos
     elif node.bf == -2:
-        rotateLeft(t, node)
+        return rotateLeft(t, node) # Balanceamos
     elif abs(node.bf) > 2:
         print(f'Node con key = {node.key} tiene un balance factor = {node.bg} incorregible!')
-    
-    return True
+        return False # Algo salió muy mal
 
 
 def reBalance(t):
     if not t:
         return None
     
-    calculateBalance(t)
-    reBalanceNode(t, t.root)
-    calculateBalance(t)
+    calculateBalance(t) # Calculamos los balances por si no existen o están desactualizados
+    reBalanceNode(t, t.root) # Balanceamos
+    calculateBalance(t) # Recalculamos los balances
     return t
 # --- end
 
@@ -283,7 +296,11 @@ def height(t):
         if node is None:
             return 0
         
+        # Utilizando el balance factor, elegimos el subárbol
+        # de mayor altura y seguimos la recursión por ahí.
         longest = node.rightnode if node.bf < 0 else node.leftnode
+        
+        # Retornamos la altura del subárbol más alto + 1
         return nodeHeight(longest) + 1
     
     return nodeHeight(t.root) - 1 if t.root else 0
