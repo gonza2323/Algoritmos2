873aee5d34c04ce55ea44fd916107d5bcbf6bb2a
Gonzalo Padilla <107971962+gonza2323@users.noreply.github.com>
2024-04-05 20:44:59 -0300

    Implement insert() in trie.py (tp3-1)

--- practicas/tp-trie/code/test.py
+++ practicas/tp-trie/code/test.py
@@ -1,2 +1,21 @@
 from trie import *
 
+t = Trie()
+
+words = [
+    "Ho",
+    "Hola",
+    "Holanda",
+    "Gonzalo",
+    "Gonza",
+    "G",
+    "Buenas",
+    "Mama",
+    "Mamadera",
+    "Ma",
+]
+
+for word in words:
+    insert(t, word)
+
+print(getWords(t))
--- practicas/tp-trie/code/trie.py
+++ practicas/tp-trie/code/trie.py
@@ -10,6 +10,58 @@ class TrieNode:
     isEndOfWord = False
 
 
+def insert(T, element):
+    if not element or not T:
+        return None
+    
+    if not T.root:
+        T.root = TrieNode()
+        T.root.children = [None] * 27
+
+    node = T.root
+    element = element.lower()
+    for i, char in enumerate(element):
+        indexChild = ord(char) - 97
+        nextNode = node.children[indexChild]
+        if not nextNode:
+            node.children[indexChild] = TrieNode()
+            nextNode = node.children[indexChild]
+            nextNode.children = [None] * 27
+            nextNode.key = char
+            nextNode.parent = node
+        if i == len(element) - 1:
+            nextNode.isEndOfWord = True
+        node = nextNode
+
+    return None
+
+
+def search(T, element):
+    return None
+
+
+def getWords(T):
+    if not T:
+        return None
+    
+    words = []
+
+    def getWordsNode(word, node):
+        if node.key:
+            word = word + node.key
+        
+        if node.isEndOfWord:
+            words.append(word)
+        
+        for child in node.children:
+            if child:
+                getWordsNode(word, child)
+    
+    if T.root:
+        getWordsNode("", T.root)
+    return words
+
+
 def toString(t):
     if not t:
         return None
