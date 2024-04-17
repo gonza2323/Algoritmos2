fdb27c4fff629aeece1315ccc5ec12147f147059
Gonzalo Padilla <107971962+gonza2323@users.noreply.github.com>
2024-04-17 01:12:36 -0300

    Delete toString function in trie.py

--- practicas/tp-trie/code/trie.py
+++ practicas/tp-trie/code/trie.py
@@ -101,37 +101,3 @@ def getWords(T):
     if T.root:
         getWordsNode("", T.root)
     return words
-
-
-# Version para árboles binarios, no funciona en trie
-def toString(t):
-    if not t:
-        return None
-    
-    def nodeToString(node, length):
-        if not node:
-            return ""
-        
-        centerString = f"({node.key}, {node.bf})"
-
-        if node.parent:
-            if node == node.parent.rightnode:
-                centerString = "┌" + centerString
-            else:
-                centerString = "└" + centerString
-        
-        if node.rightnode and node.leftnode:
-            centerString += "┤"
-        elif node.rightnode and not node.leftnode:
-            centerString += "┘"
-        elif not node.rightnode and node.leftnode:
-            centerString += "┐"
-        
-        centerString = (length - 2) * " " + centerString + "\n"
-        
-        rightString = nodeToString(node.rightnode, len(centerString))
-        leftString = nodeToString(node.leftnode, len(centerString))
-
-        return rightString + centerString + leftString
-
-    return nodeToString(t.root, 0)
