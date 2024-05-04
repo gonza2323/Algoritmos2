72f030c8af2794db554dac2d5c3de15f0f4c5ae7
Gonzalo Padilla <107971962+gonza2323@users.noreply.github.com>
2024-05-03 23:16:44 -0300

    Resolver tp4-4 y añadir pytest

--- practicas/tp-hashtable/code/hashtable.py
+++ practicas/tp-hashtable/code/hashtable.py
@@ -3,6 +3,7 @@ def hash(k, m):
     return k % m
 
 
+# --- Ejercicio 1a
 def insert(d, key, value):
     tableIndex = hash(key, len(d))
     lista = d[tableIndex]
@@ -13,13 +14,15 @@ def insert(d, key, value):
     
     for i, (k, v) in enumerate(lista):
         if k == key:
-            lista[i].value = (key, value)
+            lista[i] = (key, value)
             return d
     
     lista.append((key, value))
     return d
+# --- end
 
 
+# --- Ejercicio 1b
 def search(d, key):
     tableIndex = hash(key, len(d))
     lista = d[tableIndex]
@@ -32,8 +35,10 @@ def search(d, key):
             return v
     
     return None
+# --- end
 
 
+# --- Ejercicio 1c
 def delete(d, key):
     tableIndex = hash(key, len(d))
     lista = d[tableIndex]
@@ -49,3 +54,38 @@ def delete(d, key):
             break
     
     return d
+# --- end
+
+
+# --- Ejercicio 4
+def isPermutation(stringS, stringP):
+    if not stringS or not stringP:
+        return False
+
+    # Si son de distinto largo, no es permutación
+    if len(stringS) != len(stringP):
+        return False
+    
+    stringS = stringS.lower()
+    stringP = stringP.lower()
+    
+    # Contamos los caracteres de cada palabra
+    charsCountsS = dict()
+    charsCountsP = dict()
+    for i in range(len(stringS)):
+        if stringS[i] in charsCountsS:
+            charsCountsS[stringS[i]] += 1
+        else:
+            charsCountsS[stringS[i]] = 1
+        if stringP[i] in charsCountsP:
+            charsCountsP[stringP[i]] += 1
+        else:
+            charsCountsP[stringP[i]] = 1
+    
+    # Verificamos que coincidan
+    for c in stringS:
+        if c not in charsCountsP or charsCountsP[c] != charsCountsS[c]:
+            return False
+    
+    return True
+# --- end
---
+++ practicas/tp-hashtable/code/hashtable_test.py
@@ -0,0 +1,82 @@
+from hashtable import *
+
+
+def test_insert():
+    pairs = [
+        (0, "Gonza"),
+        (5, "Roberto"),
+        (10, "Banana"),
+        (4, "Chocolate"),
+        (9, "Frutilla"),
+        (3, "Pedro")
+    ]
+    d = [None] * 5
+    
+    for (k, v) in pairs:
+        insert(d, k, v)
+    
+    for (k, v) in pairs:
+        assert search(d, k) == v
+
+
+def test_update():
+    pairs = [
+        (0, "Gonza"),
+        (5, "Roberto"),
+        (10, "Banana"),
+        (4, "Chocolate"),
+        (9, "Frutilla"),
+        (3, "Pedro")
+    ]
+    d = [None] * 5
+    
+    for (k, v) in pairs:
+        insert(d, k, v)
+    
+    updates = [
+        (0, "Agustín"),
+        (3, "Diego"),
+        (9, "Sandía")
+    ]
+    
+    for (k, v) in updates:
+        insert(d, k, v)
+
+    for (k, v) in updates:
+        assert search(d, k) == v
+
+
+def test_delete():
+    keysToAdd = [0, 1, 2, 3, 4, 5]
+    d = [None] * 5
+    
+    for k in keysToAdd:
+        insert(d, k, k)
+    
+    keysToDelete = [0, 2, 3, 5, 10]
+
+    for k in keysToDelete:
+        delete(d, k)
+    
+    for k in keysToDelete:
+        assert search(d, k) == None
+    
+    for k in [key for key in keysToAdd if key not in keysToDelete]:
+        assert search(d, k) == k
+
+
+def test_isPermutation():
+    assert isPermutation("igual", "igual") == True
+    assert isPermutation("hola", "loha") == True
+    assert isPermutation("Hola", "loha") == True
+    assert isPermutation("gonzalo", "lozagon") == True
+    assert isPermutation("gonzalo", "lozAgOn") == True
+    assert isPermutation("asdf", "asdg") == False
+    assert isPermutation("aaa", "aba") == False
+    assert isPermutation("asdf", "asdff") == False
+    assert isPermutation("asdff", "asdf") == False
+    
+    assert isPermutation(None, "hola") == False
+    assert isPermutation("hola", None) == False
+    assert isPermutation(None, None) == False
+    assert isPermutation("", "") == False
