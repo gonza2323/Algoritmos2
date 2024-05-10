193387abfe35d6b572027a4121f190c6878afae4
Gonzalo Padilla <107971962+gonza2323@users.noreply.github.com>
2024-05-10 04:04:36 -0300

    Añadir consignas tp5

---
+++ practicas/tp-grafos/code/graph.py
@@ -0,0 +1,16 @@
+
+class graph:
+    vertices = None
+
+class vertex:
+    adjacent = None
+
+# --- Ejercicio 1
+def createGraph(vertices, edges):
+    grafo = graph()
+    
+    for edge in edges:
+        return 0
+    
+    return grafo
+# --- end
